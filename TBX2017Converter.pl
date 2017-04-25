06079da366b8aa6b7449f41c57c89cff56d57154
kadencarr <b.kaden.carr@gmail.com>
2017-04-25 16:28:36 -0600

    DOCTYPE fix, martin fix, tag name fix, etc.

--- TBX2017Converter.pl
+++ TBX2017Converter.pl
@@ -2,7 +2,8 @@
 
 use strict;
 use warnings;
-use XML::Twig
+use XML::Twig;
+use open ':encoding(utf-8)', ':std'; #this ensures output file is UTF-8
 
 # The purpose of this app is to convert TBX-Basic files into the newest standard of TBX
 # using the XML parser XML Twig. 
@@ -93,6 +94,12 @@ sub mode2
 sub program_bulk
 {
 	
+my $name = "tbx";
+my $systemD = "TBXcoreStructV03.dtd";
+my $systemB = "TBXBasiccoreStructV03.dtd";
+my $basicFlag = 0;
+my $minFlag = 0;
+my $findType;
 my $tbxMinFlag;
 my $printfile;
 my $twig_instance = XML::Twig->new(
@@ -103,9 +110,9 @@ twig_handlers => {
 	
 	# Changes for older TBX-Min files
 	
-	entry => sub { $_->set_tag( 'termEntry' ) },
+	entry => sub { $_->set_tag( 'conceptEntry' ) },
 	
-	langGroup => sub { $_->set_tag( 'langSet' ) },
+	langGroup => sub { $_->set_tag( 'langSec' ) },
 	
 	termGroup => sub { $_->set_tag( 'termSec' ) },
 	
@@ -113,18 +120,20 @@ twig_handlers => {
 				
 	TBX => sub {	my ($twig,$elt) = @_;
 					$tbxMinFlag = $elt->att("dialect");
+					$minFlag++;
 					$_->set_tag( 'tbx' );
-					$_->set_att( style => "DCT" ); 
+					$_->set_att( style => "dct" ); 
 					$_->change_att_name( 'dialect', 'type' );
 				},
 				
 	tbxMin => sub {		my ($twig,$elt) = @_;
 						$tbxMinFlag = $elt->att("dialect");
-				
 				},
 				
-	martif => sub { $_->set_tag( 'tbx' );
-					$_->set_att( style => "DCA" ); 
+	martif => sub {	my ($twig,$elt) = @_;
+					$findType = $elt->att("type");
+					$_->set_tag( 'tbx' );
+					$_->set_att( style => "dca" );
 				},
 	
 	martifHeader => sub { $_->set_tag( 'tbxHeader' ) },
@@ -137,7 +146,9 @@ twig_handlers => {
 	
 	langSet => sub { $_->set_tag( 'langSec' ) },
 	
-	tig => sub { $_->set_tag( 'termSec' ) },
+	tig => sub { $_->set_tag( 'termSec' );
+				$basicFlag++;
+ 		   	},
 	
 	termCompList => sub { $_->set_tag( 'termCompSec' ) },
 	
@@ -153,12 +164,13 @@ twig_handlers => {
 				
 				if(defined($parent))
 				{
-	            $elt->cut();
-	            $elt->paste($parent);  
+	           		$elt->cut();
+	           	 	$elt->paste($parent);  
 				}
 			},
 				
-	ntig => sub { $_->set_tag( 'termSec' ) },			
+	ntig => sub { $_->set_tag( 'termSec' );
+	 		},			
 				
 	termGrp => sub { $_->delete() },				
 	
@@ -166,8 +178,17 @@ twig_handlers => {
 
 );
 
-
 $twig_instance->parsefile($ARGV[0]);
+if($basicFlag > 0 && $minFlag == 0 && $findType eq 'TBX-Basic')
+{
+	$twig_instance->set_doctype($name, $systemB);
+}
+if($findType eq 'TBX')
+{
+	$twig_instance->set_doctype($name, $systemD);
+	my ($defaultType) = $twig_instance->findnodes('/tbx[@type]');
+	$defaultType->set_att( type => 'TBX-Default');
+}
 
 if (@ARGV == 1)
 {
